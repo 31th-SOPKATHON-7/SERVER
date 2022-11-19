e9825c95e48a9993be776db40a7be67b7deeef0a
unknown <yimcha2y0ung@gmail.com>
2022-11-20 05:52:23 +0900

    [ADD] 홈화면 조회하기

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,89 +1,40 @@
 import { Request, Response } from "express";
-import { validationResult } from "express-validator";
-import { rm, sc } from "../constants";
-import { fail, success } from "../constants/response";
-import { UserCreateDTO } from "../interfaces/UserCreateDto";
-import jwtHandler from "../modules/jwtHandler";
 import { userService } from "../service";
 
-const getUserById = async (req: Request, res: Response) => {
+const getUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
+  const user = await userService.getUser(+userId);
 
-  const data = await userService.getUserById(+userId);
-
-  if (!data) {
+  if (!user) {
     return res.status(404).json({ status: 404, message: "NOT_FOUND" });
   }
-  return res.status(200).json({ status: 200, message: "유저 조회 성공", data });
-};
 
-// 유저 정보 생성
-const createUser = async (req: Request, res: Response) => {
-
-  //? validation의 결과를 바탕으로 분기 처리
-  const error = validationResult(req);
-  if (!error.isEmpty()) {
-    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
-  }
-
-  //? 기존 비구조화 할당 방식 -> DTO의 형태
-  const userCreateDto: UserCreateDTO = req.body;
-  const data = await userService.createUser(userCreateDto);
-
-  if (!data) {
-    return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.SIGNUP_FAIL));
-  }
-
-  //? 아까 만든 jwtHandler 내 sign 함수를 이용해 accessToken 생성
-  const accessToken = jwtHandler.sign(data.id);
-
-  const result = {
-    id: data.id,
-    name: data.userName,
-    accessToken,
+  const data = {
+    user: {
+      ...user,
+    },
   };
 
-  return res.status(sc.CREATED).send(success(sc.CREATED, rm.SIGNUP_SUCCESS, data));
+  return res
+    .status(200)
+    .json({ status: 200, message: "홈 화면 조회 성공", data });
 };
 
-// 유저 정보 전체 조회
-const getAllUser = async (req: Request, res: Response) => {
-  const data = await userService.getAllUser();
-  return res.status(200).json({ status: 200, message: "유저 조회 성공", data });
-};
-
-// 유저 정보 업데이트
-const updateUser = async (req: Request, res: Response) => {
-  const { name } = req.body;
-  const { userId } = req.params;
-
-  if (!userId || !name) {
-    return res.status(400).json({ status: 400, message: "유저 정보 주세요!" });
-  }
-
-  const data = await userService.updateUser(+userId, name);
-
-  if (!data) {
-    return res.status(404).json({ status: 404, message: "유저 수정 실패" });
-  }
-  return res.status(200).json({ status: 200, message: "유저 수정 성공", data });
-};
-
-// 유저 정보 삭제
-const deleteUser = async (req: Request, res: Response) => {
-  const { userId } = req.params;
-
-  await userService.deleteUser(+userId);
-
-  return res.status(200).json({ status: 200, message: "유저 삭제 성공" });
+const getUserAll = async (req: Request, res: Response) => {
+  const user = await userService.getUserAll();
+  const data = {
+    user: {
+      ...user,
+    },
+  };
+  return res
+    .status(200)
+    .json({ status: 200, message: "홈 화면 조회 성공", data });
 };
 
 const userController = {
-  getUserById,
-  createUser,
-  getAllUser,
-  updateUser,
-  deleteUser,
+  getUser,
+  getUserAll,
 };
 
 export default userController;
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,22 +1,9 @@
 import { Router } from "express";
-import { body } from "express-validator";
 import { userController } from "../controller";
-import { auth } from "../middlewares";
 
 const router: Router = Router();
 
-router.get("/:userId", auth, userController.getUserById);
-
-// 유저 정보 생성 POST api/user
-router.post("/", [body("name").notEmpty(), body("email").notEmpty(), body("password").isLength({ min: 6 })],userController.createUser);
-
-// 전체 유저 정보 조회  GET api/user
-router.get("/", userController.getAllUser);
-
-// 유저 정보 업데이트 PATCH api/user/:userId
-router.patch("/:userId", userController.updateUser);
-
-// 유저 정보 삭제  DELETE api/user/:userId
-router.delete("/:userId", userController.deleteUser);
+router.get("/:userId", userController.getUser);
+router.get("", userController.getUserAll);
 
 export default router;
